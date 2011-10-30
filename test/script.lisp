0ddbae5cdc9e717810770347c17d8f2038e1babd
Hans Huebner <hans.huebner@gmail.com>
2011-10-30 19:01:34 +0100

    hunchentoot modern mode fixes in test script

--- test/script.lisp
+++ test/script.lisp
@@ -59,7 +59,7 @@ expecting certain responses."
     (let ((cookie-jar (funcall make-cookie-jar)))
       (http-request "cookie.html" :cookie-jar cookie-jar)
       (http-request "cookie.html" :cookie-jar cookie-jar)
-      (http-assert-body "(?ms)COOKIE-IN &quot;pumpkin&quot;.*&quot;barking&quot;"))
+      (http-assert-body "(?msi)COOKIE-IN &quot;pumpkin&quot;.*&quot;barking&quot;"))
 
     (say "Test session variables")
     (let ((cookie-jar (funcall make-cookie-jar)))
@@ -67,42 +67,42 @@ expecting certain responses."
                     :method :post :parameters '(("new-foo-value" . "ABC") ("new-bar-value" . "DEF")))
       (http-request "session.html" :cookie-jar cookie-jar)
       ;; These assertions assume that SESSION-VALUE returns the found alist value as second value
-      (http-assert-body "\(HUNCHENTOOT-TEST::FOO . &quot;ABC&quot;\)")
-      (http-assert-body "\(HUNCHENTOOT-TEST::BAR . &quot;DEF&quot;\)"))
+      (http-assert-body "(?i)\(HUNCHENTOOT-TEST::FOO . &quot;ABC&quot;\)")
+      (http-assert-body "(?i)\(HUNCHENTOOT-TEST::BAR . &quot;DEF&quot;\)"))
 
     (say "Test GET parameters with foreign characters (Latin-1)")
     (http-request "parameter_latin1_get.html"
                   :external-format-out :iso-8859-1
                   :parameters (list (cons "foo" (format nil "H~Chner" #.(code-char 252))))
                   :additional-headers '(("Content-Type" . "text/plain; charset=iso-8859-1")))
-    (http-assert-header :content-type "text/html; charset=ISO-8859-1")
+    (http-assert-header :content-type "(?i)text/html; charset=ISO-8859-1")
     (http-assert-body "(72 252 104 110 101 114)")
-    (http-assert-body "&quot;H&#xFC;hner&quot;")
+    (http-assert-body "(?i)&quot;H&#xFC;hner&quot;")
 
     (say "Test POST parameters with foreign characters (Latin-1)")
     (http-request "parameter_latin1_post.html"
                   :external-format-out :iso-8859-1
                   :method :post :parameters (list (cons "foo" (format nil "H~Chner" #.(code-char 252)))))
-    (http-assert-header :content-type "text/html; charset=ISO-8859-1")
+    (http-assert-header :content-type "(?i)text/html; charset=ISO-8859-1")
     (http-assert-body "(72 252 104 110 101 114)")
-    (http-assert-body "&quot;H&#xFC;hner&quot;")
+    (http-assert-body "(?i)&quot;H&#xFC;hner&quot;")
 
     (say "Test GET parameters with foreign characters (UTF-8)")
     (http-request "parameter_utf8_get.html"
                   :external-format-out :utf-8
                   :parameters (list (cons "foo" (format nil "H~Chner" #.(code-char 252)))))
-    (http-assert-header :content-type "text/html; charset=UTF-8")
+    (http-assert-header :content-type "(?i)text/html; charset=UTF-8")
     (http-assert-body "(72 252 104 110 101 114)")
-    (http-assert-body "&quot;H&#xFC;hner&quot;")
+    (http-assert-body "(?i)&quot;H&#xFC;hner&quot;")
 
     (say "Test POST parameters with foreign characters (UTF-8)")
     (http-request "parameter_utf8_post.html"
                   :method :post
                   :external-format-out :utf-8
                   :parameters (list (cons "foo" (format nil "H~Chner" #.(code-char 252)))))
-    (http-assert-header :content-type "text/html; charset=UTF-8")
+    (http-assert-header :content-type "(?i)text/html; charset=UTF-8")
     (http-assert-body "(72 252 104 110 101 114)")
-    (http-assert-body "&quot;H&#xFC;hner&quot;")
+    (http-assert-body "(?i)&quot;H&#xFC;hner&quot;")
 
     (say "Test redirection")
     (http-request "redir.html")
