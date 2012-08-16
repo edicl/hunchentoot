68b56341bc81fa4d97bdab13d1a624cd42102595
Hans Huebner <hans.huebner@gmail.com>
2012-08-16 16:02:46 +0200

    improve comment regarding handler thread creation failure

--- taskmaster.lisp
+++ taskmaster.lisp
@@ -367,7 +367,13 @@ is set up via PROCESS-REQUEST."
         (decrement-taskmaster-request-count taskmaster)))
     :name (format nil (taskmaster-worker-thread-name-format taskmaster) (client-as-string socket)))
    (error (cond)
-          ;; need to decrement the request count because we didn't reach the unwind-protect body.
+          ;; The error occured during before the request handling
+          ;; thread was actually created - Most likely in the
+          ;; client-as-string function which potentially creates
+          ;; network errors because it tries to determine the address
+          ;; of the client.  Therefore, the unwind-protect in the
+          ;; handler thread above will never be executed and we need
+          ;; to do it here.
           (decrement-taskmaster-request-count taskmaster)
           ;; need to bind *ACCEPTOR* so that LOG-MESSAGE* can do its work.
           (let ((*acceptor* (taskmaster-acceptor taskmaster)))
