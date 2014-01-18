522314faf0d9ed3dd2ce65e04494bdecb555829d
Stas Boukarev <stassats@gmail.com>
2014-01-18 23:32:49 +0400

    Close SSL streams after processing connection.
    Otherwise it leaks foreign memory.
    
    Reported by José Ronquillo.

--- acceptor.lisp
+++ acceptor.lisp
@@ -372,55 +372,63 @@ they're using secure connections - see the SSL-ACCEPTOR class."))
                      :server-protocol server-protocol))))
 
 (defmethod process-connection ((*acceptor* acceptor) (socket t))
-  (let ((*hunchentoot-stream* (make-socket-stream socket *acceptor*)))
+  (let* ((socket-stream (make-socket-stream socket *acceptor*))
+         (*hunchentoot-stream* (initialize-connection-stream *acceptor* socket-stream)))
     (unwind-protect
-        ;; process requests until either the acceptor is shut down,
-        ;; *CLOSE-HUNCHENTOOT-STREAM* has been set to T by the
-        ;; handler, or the peer fails to send a request
-        (let ((*hunchentoot-stream* (initialize-connection-stream *acceptor* *hunchentoot-stream*)))
-          (loop
-             (let ((*close-hunchentoot-stream* t))
-               (when (acceptor-shutdown-p *acceptor*)
-                 (return))
-               (multiple-value-bind (headers-in method url-string protocol)
-                   (get-request-data *hunchentoot-stream*)
-                 ;; check if there was a request at all
-                 (unless method
-                   (return))
-                 ;; bind per-request special variables, then process the
-                 ;; request - note that *ACCEPTOR* was bound above already
-                 (let ((*reply* (make-instance (acceptor-reply-class *acceptor*)))
-                       (*session* nil)
-                       (transfer-encodings (cdr (assoc* :transfer-encoding headers-in))))
-                   (when transfer-encodings
-                     (setq transfer-encodings
-                           (split "\\s*,\\s*" transfer-encodings))
-                     (when (member "chunked" transfer-encodings :test #'equalp)
-                       (cond ((acceptor-input-chunking-p *acceptor*)
-                              ;; turn chunking on before we read the request body
-                              (setf *hunchentoot-stream* (make-chunked-stream *hunchentoot-stream*)
-                                    (chunked-stream-input-chunking-p *hunchentoot-stream*) t))
-                             (t (hunchentoot-error "Client tried to use ~
+         ;; process requests until either the acceptor is shut down,
+         ;; *CLOSE-HUNCHENTOOT-STREAM* has been set to T by the
+         ;; handler, or the peer fails to send a request
+         (loop
+          (let ((*close-hunchentoot-stream* t))
+            (when (acceptor-shutdown-p *acceptor*)
+              (return))
+            (multiple-value-bind (headers-in method url-string protocol)
+                (get-request-data *hunchentoot-stream*)
+              ;; check if there was a request at all
+              (unless method
+                (return))
+              ;; bind per-request special variables, then process the
+              ;; request - note that *ACCEPTOR* was bound above already
+              (let ((*reply* (make-instance (acceptor-reply-class *acceptor*)))
+                    (*session* nil)
+                    (transfer-encodings (cdr (assoc* :transfer-encoding headers-in))))
+                (when transfer-encodings
+                  (setq transfer-encodings
+                        (split "\\s*,\\s*" transfer-encodings))
+                  (when (member "chunked" transfer-encodings :test #'equalp)
+                    (cond ((acceptor-input-chunking-p *acceptor*)
+                           ;; turn chunking on before we read the request body
+                           (setf *hunchentoot-stream* (make-chunked-stream *hunchentoot-stream*)
+                                 (chunked-stream-input-chunking-p *hunchentoot-stream*) t))
+                          (t (hunchentoot-error "Client tried to use ~
 chunked encoding, but acceptor is configured to not use it.")))))
-                   (with-acceptor-request-count-incremented (*acceptor*)
-                     (process-request (acceptor-make-request *acceptor* socket
-                                                             :headers-in headers-in
-                                                             :content-stream *hunchentoot-stream*
-                                                             :method method
-                                                             :uri url-string
-                                                             :server-protocol protocol))))
-                 (finish-output *hunchentoot-stream*)
-                 (setq *hunchentoot-stream* (reset-connection-stream *acceptor* *hunchentoot-stream*))
-                 (when *close-hunchentoot-stream*
-                   (return))))))
-      (when *hunchentoot-stream*
+                (with-acceptor-request-count-incremented (*acceptor*)
+                  (process-request (acceptor-make-request *acceptor* socket
+                                                          :headers-in headers-in
+                                                          :content-stream *hunchentoot-stream*
+                                                          :method method
+                                                          :uri url-string
+                                                          :server-protocol protocol))))
+              (finish-output *hunchentoot-stream*)
+              (setq *hunchentoot-stream* (reset-connection-stream *acceptor* *hunchentoot-stream*))
+              (when *close-hunchentoot-stream*
+                (return)))))
+      (unless (eql socket-stream *hunchentoot-stream*)
         ;; as we are at the end of the request here, we ignore all
         ;; errors that may occur while flushing and/or closing the
         ;; stream.
         (ignore-errors*
           (finish-output *hunchentoot-stream*))
         (ignore-errors*
-          (close *hunchentoot-stream* :abort t))))))
+          (close *hunchentoot-stream* :abort t)))
+      (when socket-stream
+        ;; as we are at the end of the request here, we ignore all
+        ;; errors that may occur while flushing and/or closing the
+        ;; stream.
+        (ignore-errors*
+          (finish-output socket-stream))
+        (ignore-errors*
+          (close socket-stream :abort t))))))
 
 (defmethod acceptor-ssl-p ((acceptor t))
   ;; the default is to always answer "no"
