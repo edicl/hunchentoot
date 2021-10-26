03f7ce29f45980391ae5fc9b973043d2f36dfdfb
Fabien Pelletier <fabien@fabienpelletier.com>
2021-10-26 17:19:00 -0400

    naive first pass at adding SameSite attribute for cookies

--- cookie.lisp
+++ cookie.lisp
@@ -56,11 +56,17 @@ cookie expires \(or NIL).")
            :initform nil
            :accessor cookie-domain
            :documentation "The domain this cookie is valid for \(or NIL).")
+   (same-site :initarg :same-site
+           :initform "None"
+           :accessor cookie-same-site
+           :documentation "The SameSite attribute for the cookie, needs
+to be one of \"None\", \"Lax\" or \"Strict\". See 
+<https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-02#section-5.3.7>.")
    (secure :initarg :secure
            :initform nil
            :accessor cookie-secure
            :documentation "A generalized boolean denoting whether this
-cookie is a secure cookie.")
+cookie is a secure cookie.")   
    (http-only :initarg :http-only
               :initform nil
               :accessor cookie-http-only
@@ -91,7 +97,7 @@ REPLY object REPLY. If a cookie with the same name
         (push (cons name cookie) (cookies-out reply))
         cookie))))
 
-(defun set-cookie (name &key (value "") expires max-age path domain secure http-only (reply *reply*))
+(defun set-cookie (name &key (value "") expires max-age path domain same-site secure http-only (reply *reply*))
   "Creates a cookie object from the parameters provided and adds
 it to the outgoing cookies of the REPLY object REPLY. If a cookie
 with the name NAME \(case-sensitive) already exists, it is
@@ -103,6 +109,7 @@ replaced."
                               :max-age max-age
                               :path path
                               :domain domain
+                              :same-site same-site
                               :secure secure
                               :http-only http-only)
                reply))
@@ -123,5 +130,6 @@ replaced."
           (cookie-max-age cookie)
           (cookie-domain cookie)
           (cookie-path cookie)
+          (cookie-same-site cookie)
           (cookie-secure cookie)
           (cookie-http-only cookie)))
